789240b53601a46cd0f26e5c14199129fb49decc
Erik Paldanius <erik.paldanius@gmail.com>
2015-12-12 19:30:08 +0100

    Removed some stuff

--- LD34/Cloud.cs
+++ LD34/Cloud.cs
@@ -19,26 +19,18 @@ namespace LD34
     {
         public Cloud()
         {
-            Depth = 5f + Engine.RandomFloat() * 20f;
-
-            do
-            {
-                LocalCoord = Scene.CameraBounds.TopLeft + new Vector2(4000f, 800f) * new Vector2(Engine.RandomFloat(), Engine.RandomFloat());
-            } while (Scene.EnumerateObjects<Cloud>().Select(o => o == this ? 9999f : (o.LocalCoord - LocalCoord).Length).Min() < 300f);
-
+            Depth = 5f; // + Engine.RandomFloat() * 20f;
+            
             Texture texture = Resources.GetTexture("cloud" + (1 + (int)(Engine.RandomFloat() * 3f)).ToString());
             SpriteComponent.RegisterOn(this, texture);
             Size = texture.Size;
-
-            RegisterEvent<PhysicsUpdateEvent>(0, PhysicsUpdate);
         }
 
         public void PhysicsUpdate(PhysicsUpdateEvent ev)
         {
-            LocalCoord += new Vector2(Depth * ev.DeltaTime, 0f);
-            if (LocalCoord.X >= Scene.CameraBounds.Right)
+            if (WorldCoord.X - Scene.Camera.WorldCoord.X + Engine.Resolution.X / 2f < -300f)
             {
-                LocalCoord = new Vector2(0f, LocalCoord.Y);
+                Remove();
             }
         }
     }
--- LD34/MainScene.cs
+++ LD34/MainScene.cs
@@ -1,4 +1,5 @@
 ﻿using Cog;
+using Cog.Modules.Content;
 using Cog.Modules.EventHost;
 using Cog.Scenes;
 using System;
@@ -12,34 +13,32 @@ namespace LD34
     class MainScene : Scene
     {
         Vector2 cameraPosition;
-        const float CameraSpeed = 6f;
-
-        public Player OurPlayer;
-
-        public Rectangle CameraBounds = new Rectangle(new Vector2(0f, -1000f), new Vector2(4000f, 1200f));
+        float cloudSpawnX;
 
         public MainScene()
             : base("Main Scene")
         {
             DrawAllObjects = true;
+            cameraPosition = new Vector2(0f, -200f);
+            Camera.WorldCoord = cameraPosition;
 
             BackgroundColor = new Color(49, 162, 242);
             CreateLocalObject<World>(Vector2.Zero);
-            OurPlayer = CreateLocalObject<Player>(new Vector2(1000f, -500f));
 
-            for (int i=0; i<20; i++)
-            {
-                CreateLocalObject<Cloud>(Vector2.Zero);
-            }
+            cloudSpawnX = -Engine.Resolution.X / 2f;
 
             RegisterEvent<PhysicsUpdateEvent>(0, PhysiscUpdate);
         }
 
         public void PhysiscUpdate(PhysicsUpdateEvent ev)
         {
-            cameraPosition += (OurPlayer.WorldCoord - cameraPosition) * Mathf.Min(1f, ev.DeltaTime * CameraSpeed);
-            cameraPosition.X = Mathf.Max(CameraBounds.Left, Mathf.Min(CameraBounds.Right - Engine.Resolution.X, cameraPosition.X - Engine.Resolution.X / 2f)) + Engine.Resolution.X / 2f;
-            cameraPosition.Y = Mathf.Max(CameraBounds.Top, Mathf.Min(CameraBounds.Bottom - Engine.Resolution.Y, cameraPosition.Y - Engine.Resolution.Y / 2f)) + Engine.Resolution.Y / 2f;
+            while (cloudSpawnX < Camera.WorldCoord.X + Engine.Resolution.X / 2f + 300f)
+            {
+                CreateLocalObject<Cloud>(new Vector2(cloudSpawnX, -500f + Engine.RandomFloat() * 200f));
+                cloudSpawnX += 300f + Engine.RandomFloat() * 200f;
+            }
+
+            cameraPosition += new Vector2(600f * ev.DeltaTime, 0f);
             Camera.WorldCoord = cameraPosition.Floor;
         }
     }
