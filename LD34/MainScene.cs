ae4c7b2f9260173eb02aa09f78e31617935ad36a
Erik Paldanius <erik.paldanius@gmail.com>
2015-12-12 11:25:44 +0100

    created project and set up engine

---
+++ LD34/MainScene.cs
@@ -0,0 +1,18 @@
+﻿using Cog.Scenes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LD34
+{
+    class MainScene : Scene
+    {
+        public MainScene()
+            : base("Main Scene")
+        {
+
+        }
+    }
+}
---
+++ LD34/Program.cs
@@ -0,0 +1,27 @@
+﻿using Cog;
+using Cog.Modules.EventHost;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LD34
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Engine.Initialize<XnaRenderer.XnaRenderer, IrrKlangModule.IrrKlangAudioModule>();
+
+            Engine.DesiredResolution = new Vector2(1280f, 720f);
+
+            Engine.EventHost.RegisterEvent<InitializeEvent>(0, ev =>
+            {
+                Engine.SceneHost.Push(Engine.SceneHost.CreateLocal<MainScene>());
+            });
+
+            Engine.StartGame("Ludum Dare 34", Cog.Modules.Renderer.WindowStyle.Default);
+        }
+    }
+}
